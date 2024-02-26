e895541fefaf243b747db8fb7777334c1bcf9831
Ivo Bellin Salarin <me@nilleb.com>
2024-02-26 09:06:45 +0100

    fix: site url

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://blog.nilleb.com"
+SITEURL = "https://nilleb.github.io"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
